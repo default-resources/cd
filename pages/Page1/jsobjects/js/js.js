919e91f38496961967322d83791808bbeef6ae91
manish <manish@appsmith.com>
2024-11-11 14:45:36 +0530

    commiting with a page load action

--- pages/Page1/jsobjects/js/js.js
+++ pages/Page1/jsobjects/js/js.js
@@ -2,8 +2,7 @@ export default {
 	myVar1: [],
 	myVar2: {},
 	myFun1 () {
-		//	write code here
-		//	this.myVar1 = [1,2,3]
+		return "Text value from js on page load";
 	},
 	async myFun2 () {
 		//	use async-await or promises
